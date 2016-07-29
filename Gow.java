8baad24aed83ed90ed28afb71f7b3d061808f00d
GOWREESWARI23 <gowrisubramanianfire@gmail.com>
2016-07-29 10:27:55 +0530

    Create Gow.java

---
+++ Gow.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Gow
+{
+public static void main(String args[])
+{
+String s,reverse="  ";
+Scanner in=new Scanner(System.in);
+s=in.next();
+System.out.println(s);
+String reverse1=new reverse(s);
+reverse.toString();
+System.out.println(s+reverse);
+}
+}
